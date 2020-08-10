d2d1a57ee6f77164f80f7922fefc4cc5172e944c
Eric Hillenbrand <ehillenbrand@r1rcm.com>
2020-08-10 09:33:22 -0600

    adding server

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
+const SessionAPI = require('./datasources/sessions');
 
 const typeDefs = gql`
   type Query {
     sessions: [Session]
+    sessionById(id: ID): Session
   }
   type Session {
     id: ID!
@@ -13,12 +15,27 @@ const typeDefs = gql`
     room: String
     day: String
     format: String
-    track: String
+    track: String @deprecated(reason: "Too many sessions do not fit")
     level: String
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    sessions: (parent, args, { dataSources }, info) => {
+      return dataSources.SessionAPI.getSessions();
+    },
+    sessionById: (parent, { id }, { dataSources }, info) => {
+      return dataSources.SessionAPI.getSessionById(id);
+    },
+  },
+};
+
+const dataSources = () => ({
+  SessionAPI: new SessionAPI(),
+});
+
+const server = new ApolloServer({ typeDefs, resolvers, dataSources });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`graphQL running at ${url}`);
